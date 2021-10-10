7df4f74cf4ed97bf91001ba4154574353eaf5e7a
Javed Ali <30997178+MJavedAli@users.noreply.github.com>
2021-10-11 00:04:31 +0530

    Create diamond.java

---
+++ diamond.java
@@ -0,0 +1,91 @@
+package patterns;
+
+public class Barfi {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int i, j; 
+		
+		
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=2*n;j++) {
+				if(i>(n-j+1)) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+				
+				if(j>=n+i) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=2*n;j++) {
+				if(j>i) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+				
+				if(j<=2*n-i) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+
+		
+		
+		
+		
+		
+		
+
+		
+ }
+}
+
+
+
+/*
+for(i=1;i<=n;i++) {
+			for(j=1;j<=2*n;j++) {			
+				if(i>(n-j+1)) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+				
+				if((n+i)>j) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}	
+			}
+			System.out.println();
+		}
+		
+		
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=2*n;j++) {			
+				if(i<j) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+				
+				if((2*n-i)>=j) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}	
+			}
+			System.out.println();
+		}
+*/
